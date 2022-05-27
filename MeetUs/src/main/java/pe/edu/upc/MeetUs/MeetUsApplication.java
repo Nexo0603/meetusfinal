f5aeb23a2a3acededf22378c6060f9dca04e61b6
Nexo0603 <u201624018@upc.edu.pe>
2022-05-26 21:17:17 -0500

    Master

---
+++ MeetUs/src/main/java/pe/edu/upc/MeetUs/MeetUsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.MeetUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetUsApplication.class, args);
+	}
+
+}
