1e2ce63a61af25b8731cc456acb407d3178071ea
Cesar Cardenas-Villalba <u201723401@upc.edu.pe>
2022-06-06 15:11:40 -0500

    Crud Organizer

--- MeetUs/src/main/java/pe/edu/upc/MeetUs/controller/OrganizerController.java
+++ MeetUs/src/main/java/pe/edu/upc/MeetUs/controller/OrganizerController.java
@@ -1,12 +1,14 @@
 package pe.edu.upc.MeetUs.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -53,6 +55,58 @@ public class OrganizerController {
 			Organizer organizerSaved = organizerService.create(organizer);
 		} catch (Exception e) {
 			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return "redirect:/organizers";
+	}
+	
+	@GetMapping("{id}/edit")	
+	public String editOrganizer(Model model, @PathVariable("id") String id) {				
+		try {
+			if (organizerService.existsById(id)) {
+				Optional<Organizer> optional = organizerService.findById(id);
+				model.addAttribute("organizer", optional.get());
+				List<Organizer> organizers = organizerService.getAll();
+				model.addAttribute("organizers", organizers);
+			} else {
+				return "redirect:/organizers";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "organizers/edit-organizer";
+	}
+	
+	@PostMapping("{id}/update")	
+	public String updateOrganizer(Model model, @ModelAttribute("organizer") Organizer organizer, 
+			@PathVariable("id") String id) {
+		try {
+			if (organizerService.existsById(id)) {
+				organizerService.update(organizer);
+			} else {
+				return "redirect:/organizers";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/organizers";
+	}
+	
+	@GetMapping("{id}/del")	
+	public String deleteOrganizer(Model model, @PathVariable("id") String id) {
+		try {
+			if (organizerService.existsById(id)) {
+				organizerService.deleteById(id);
+			} else {
+				return "redirect:/organizers";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return "redirect:/organizers";
