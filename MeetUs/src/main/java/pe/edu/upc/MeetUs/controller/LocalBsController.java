cde921fa9e4b795160ca419e972a3ad228c9183d
Nexo0603 <u201624018@upc.edu.pe>
2022-06-19 21:12:33 -0500

    HTML PARTE 3 LOGICA DE NEGOCIO (LOCAL)

---
+++ MeetUs/src/main/java/pe/edu/upc/MeetUs/controller/LocalBsController.java
@@ -0,0 +1,122 @@
+package pe.edu.upc.MeetUs.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import pe.edu.upc.MeetUs.business.crud.LocalService;
+import pe.edu.upc.MeetUs.business.crud.LocalOwnerService;
+import pe.edu.upc.MeetUs.models.entities.Local;
+import pe.edu.upc.MeetUs.models.entities.LocalOwner;
+
+@Controller
+@RequestMapping("/locals-bs")	// GET y POST
+@SessionAttributes("{local}")
+public class LocalBsController {
+
+	@Autowired
+	private LocalService localService;
+	
+	@Autowired
+	private LocalOwnerService localownerService; 
+
+	@GetMapping		//	/locals
+	public String listLocals(Model model) {
+		
+		try {
+			List<Local> locals = localService.getAll();
+			model.addAttribute("locals", locals);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "locals-bs/list-locals";
+	}
+	
+	@GetMapping("new")	//	/locals/new
+	public String newLocal(Model model) {
+		Local local = new Local();
+		model.addAttribute("local", local);
+		try {
+			List<LocalOwner> localowners = localownerService.getAll();
+			model.addAttribute("localowners", localowners);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "locals-bs/new-local";
+	}
+	
+	@PostMapping("savenew")	//	/locals/savenew
+	public String saveLocal(Model model, @ModelAttribute("local") Local local) {
+		try {
+			Local localSaved = localService.create(local);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/locals-bs";
+	}
+	
+	@GetMapping("{id}/edit")	//	/locals/1/edit
+	public String editLocal(Model model, @PathVariable("id") Integer id) {				
+		try {
+			if (localService.existsById(id)) {
+				Optional<Local> optional = localService.findById(id);
+				model.addAttribute("local", optional.get());
+				List<LocalOwner> localowners = localownerService.getAll();
+				model.addAttribute("localowners", localowners);
+			} else {
+				return "redirect:/locals-bs";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "locals-bs/edit-local";
+	}
+	
+	@PostMapping("{id}/update")	//	/locals/1/update
+	public String updateLocal(Model model, @ModelAttribute("local") Local local, 
+			@PathVariable("id") Integer id) {
+		try {
+			if (localService.existsById(id)) {
+				localService.update(local);
+			} else {
+				return "redirect:/locals-bs";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/locals-bs";
+	}
+	
+	@GetMapping("{id}/del")	//	/locals/1/del
+	public String deleteLocal(Model model, @PathVariable("id") Integer id) {
+		try {
+			if (localService.existsById(id)) {
+				localService.deleteById(id);
+			} else {
+				return "redirect:/locals-bs";
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/locals-bs";
+	}
+}
